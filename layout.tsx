340ea13a89cea060a8fb3e314a8c7c290f17350a
asd <ppdew9811@gmail.com>
2025-11-10 23:04:12 +0700

    Create layout.tsx

---
+++ layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import ClientBody from "./ClientBody";
+import Script from "next/script";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Jurnalify - Learn the Paper, Not the Pain",
+  description: "Platform AI untuk memahami jurnal akademik dengan cepat dan mudah. Meringkas, menjelaskan, dan memvisualisasikan isi jurnal dalam hitungan detik.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="id" className={`${geistSans.variable} ${geistMono.variable} scroll-smooth`}>
+      <head>
+        <Script
+          crossOrigin="anonymous"
+          src="//unpkg.com/same-runtime/dist/index.global.js"
+        />
+      </head>
+      <body suppressHydrationWarning className="antialiased">
+        <ClientBody>{children}</ClientBody>
+      </body>
+    </html>
+  );
+}
